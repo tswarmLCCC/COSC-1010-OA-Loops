a232e89071eb8b5916c14d94a756fd2068a59ba3
Trevor Swarm <tswarm@lccc.wy.edu>
2025-09-19 15:37:40 -0600

    Create 1_Predict.py with activity predictions

---
+++ 1_Predict.py
@@ -0,0 +1,42 @@
+"""
+Predict Activity 1: Counting Up
+
+Task:
+Read the code below. Without running it, what do you think the output will be?
+Write down your prediction and a short reason for it.
+
+Think about these questions:
+- What does the range(5) function do?
+- What numbers will the variable 'i' hold?
+- How many times will the print statement run?
+"""
+
+print("Getting ready to count...")
+
+for i in range(5):
+    print(i)
+
+print("All done!")
+
+
+"""
+Predict Activity 2: Countdown
+
+Task:
+Look at this 'while' loop. What will it print to the screen?
+Write down what you think the final output will be.
+
+Think about these questions:
+- What is the starting value of 'counter'?
+- What condition is the 'while' loop checking?
+- What happens to the value of 'counter' inside the loop?
+- When will the loop stop?
+"""
+
+counter = 3
+
+while counter > 0:
+    print(f"T-minus {counter}...")
+    counter = counter - 1
+
+print("Blast off!")
